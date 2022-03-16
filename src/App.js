cf70b8941b6ebeb1383e103fad3cf5e7fbcdbbd5
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-16 10:17:45 +0100

    flashcards and users in json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import flashcards from './data/flashcards.json';
+import config from './data/config.json';
+import users from './data/users.json';
 
 const title = "The Info Site";
 const colors = ['yellow', 'orange', 'purple', 'black'];
-const config = {
-	status: 'online',
-	os: 'windows'
-};
 
 function App() {
 	return (
@@ -20,6 +19,23 @@ function App() {
 				})}
 			</ul>
 			<p>Status: {config.status}</p>
+
+			<h2>Flaschards</h2>
+			<ul>
+				{flashcards.map(flashcard => {
+					return (
+						<li>{flashcard.front}</li>
+					)
+				})}
+			</ul>
+			<h2>Users</h2>
+			<ul>
+				{users.map(user => {
+					return (
+						<li>{user.firstName} {user.lastName}</li>
+					)
+				})}
+			</ul>
 		</div>
 	);
 }
