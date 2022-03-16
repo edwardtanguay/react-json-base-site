938d2a4d493b1b7599649459e1dd6ba2dda51db0
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-16 10:03:25 +0100

    showing javascript code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 
+const title = "The Info Site";
+const colors = ['yellow', 'orange', 'purple', 'black'];
+const config = {
+	status: 'online',
+	os: 'windows'
+};
+
 function App() {
-  return (
-    <div className="App">
-		  <strong>Test</strong>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>{title}</h1>
+			<p>This is a test.</p>
+			<ul>
+				{colors.map(color => {
+					return (
+						<li>{color}</li>
+					)
+				})}
+			</ul>
+			<p>Status: {config.status}</p>
+		</div>
+	);
 }
 
 export default App;
